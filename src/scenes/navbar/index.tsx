c0e782e4893291084d557c23def83c22ef803355
Naome <tuyishimenaome27@gmail.com>
2023-08-05 10:54:39 +0300

    end

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -3,6 +3,8 @@ import {Bars3Icon, XMarkIcon} from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
 import Link from "./Link"
 import { SelectedPage } from '@/shared/types';
+import useMediaQuery from '@/hooks/useMediaQuery';
+import ActionButton from '@/shared/ActionButton';
 
 interface Props {
     selectedPage:SelectedPage;
@@ -11,6 +13,8 @@ interface Props {
 
 const Navbar = ({selectedPage,setSelectedPage}: Props) => {
     const flexBetween="flex items-center justify-between"
+    const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
+    const [isMenuToggled,setIsMenuToggled] = useState<boolean>(false);
     return (
        <nav>
         <div
@@ -20,8 +24,9 @@ const Navbar = ({selectedPage,setSelectedPage}: Props) => {
                 <div className={`${flexBetween} w-full gap-16`}>
                     {/* left side */}
                     <img src={Logo} alt="logo" />
+                    
                     {/* Right side */}
-                    <div className={`${flexBetween} w-full`}>
+                    { isAboveMediumScreens ? (<div className={`${flexBetween} w-full`}>
                         <div className={`${flexBetween} gap-8 text-sm`}>
                             <Link 
                             page="Home"
@@ -45,9 +50,17 @@ const Navbar = ({selectedPage,setSelectedPage}: Props) => {
                         </div>
                         <div className={`${flexBetween} gap-8`}>
                             <p>Sign In</p>
-                            <button>Become a member</button>
+                            <ActionButton>Become a member</ActionButton>
                         </div>
-                    </div>
+                    </div> 
+                    ): (
+                        <button
+                        className='rounded-full bg-secondary-500 p-2'
+                        onClick={() => setIsMenuToggled(!isMenuToggled)}
+                        >
+                            <Bars3Icon className='h-6 w-6 text-white'/>
+                        </button>
+                    )}
                 </div>
             </div>
         </div>
