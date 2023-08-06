dbaa8d1ff17ec269284f91f8de5c9c32bb971ef5
Naome <tuyishimenaome27@gmail.com>
2023-08-06 23:11:02 +0300

    end

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import Navbar from "@/scenes/navbar/index.tsx";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { SelectedPage } from "./shared/types";
-
+ 
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+  const [isTopOfPage,setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(()=>{
+    const handleScroll =()=>{
+      if(window.scrollY === 0){
+        setIsTopOfPage(true);
+
+        setSelectedPage(SelectedPage.Home)
+      }
+    }
+  })
 
   return (
    <div className='app bg-gray-20'>
