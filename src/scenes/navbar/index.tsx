78f263f19d19bab140e4c45b3dfeb683957cd430
Naome <tuyishimenaome27@gmail.com>
2023-08-06 23:11:17 +0300

    end

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -64,7 +64,40 @@ const Navbar = ({selectedPage,setSelectedPage}: Props) => {
                 </div>
             </div>
         </div>
-        
+        {/* Mobile Menu Modal */}
+        {!isAboveMediumScreens && isMenuToggled && (
+            <div className='fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl'>
+                {/* CLose Icon */}
+                <div className='flex justify-end p-12'>
+                    <button onClick={()=> setIsMenuToggled(!isMenuToggled)}>
+                        <XMarkIcon className='h-6 w-6 text-gray-400'/> 
+                    </button>
+                </div>
+
+                {/* Menu items */} 
+                <div className='ml-[33%] flex flex-col gap-10 text-2xl'>
+                            <Link 
+                            page="Home"
+                            selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                            />
+                            <Link  
+                            page="Benefits"
+                            selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                            />
+                            <Link 
+                            page="Our Classes"
+                            selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}/>
+                            <Link 
+                            page="Contact Us"
+                            selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                            />
+                        </div>
+            </div>
+        )}
        </nav>
     )
 }
