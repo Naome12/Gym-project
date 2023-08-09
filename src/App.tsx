75801094bbbc03b9798d94a992a9368263976b1e
Naome <tuyishimenaome27@gmail.com>
2023-08-09 23:04:12 +0300

    end

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,44 @@
-import Navbar from "@/scenes/navbar/index.tsx";
-import { useState,useEffect } from "react";
-import { SelectedPage } from "./shared/types";
- 
+import Navbar from "@/scenes/navbar";
+import Home from "@/scenes/home";
+import OurClasses from "@/scenes/ourClasses";
+import Benefits from "@/scenes/benefits";
+import ContactUs from "@/scenes/contactUs";
+import Footer from "@/scenes/footer";
+import { useEffect, useState } from "react";
+import { SelectedPage } from "@/shared/types";
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
-  const [isTopOfPage,setIsTopOfPage] = useState<boolean>(true);
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
 
-  useEffect(()=>{
-    const handleScroll =()=>{
-      if(window.scrollY === 0){
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
         setIsTopOfPage(true);
-
-        setSelectedPage(SelectedPage.Home)
+        setSelectedPage(SelectedPage.Home);
       }
-      if(window.scrollY !== 0) setIsTopOfPage (false);
-    }
-  })
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-   <div className='app bg-gray-20'>
-    <Navbar
-      selectedPage={selectedPage}
-      setSelectedPage={setSelectedPage}
+    <div className="app bg-gray-20">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
       />
-   </div>
-  )
+      <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
